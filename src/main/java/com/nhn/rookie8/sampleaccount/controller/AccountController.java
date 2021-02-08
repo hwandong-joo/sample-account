287446748b893dc641049537b95ad3131f046f55
hwandong-joo <hwandong-joo@godo.co.kr>
2021-02-08 16:46:59 +0900

    add

---
+++ src/main/java/com/nhn/rookie8/sampleaccount/controller/AccountController.java
@@ -0,0 +1,15 @@
+package com.nhn.rookie8.sampleaccount.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+    @GetMapping("/helloworld")
+    public String helloworld(){
+        return "helloworld";
+    }
+}
